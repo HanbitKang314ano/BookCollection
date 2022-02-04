ec6a7cf9ec552ec2cd7b000d68c41a209097d796
Hanbit Kang <hk24536@tamu.edu>
2022-02-03 22:43:19 -0600

    initial commit

---
+++ db/migrate/20220204033842_addnewattributes.rb
@@ -0,0 +1,7 @@
+class Addnewattributes < ActiveRecord::Migration[6.1]
+  def change
+    add_column :books, :author, :string
+    add_column :books, :price, :numeric
+    add_column :books, :published_date, :date
+  end
+end
